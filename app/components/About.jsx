c3a12074f637714516220f66e5f2e4c81d7a5e79
Ross Gordon <rossagordonstl@gmail.com>
2023-08-15 15:30:05 -0400

    fix issue with hero video not playing

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="relative bg-black md:h-[75vh] h-[70vh] flex justify-center items-center text-center sm:text-left px-4 sm:px-0 -mt-0.5">
-      <div className="z-10 flex flex-col items-center justify-center space-y-4 sm:space-y-6 md:space-y-8 lg:space-y-10 w-full">
-        <p className="text-center text-white text-6xl md:text-8xl font-bold">
-          Motivation You Wear.
+    <div className="relative bg-black md:h-[75vh] h-[70vh] flex justify-center items-center text-center sm:text-left px-4 sm:px-0 -mt-0.5 overflow-hidden">
+      <div className="z-10 flex flex-col items-center justify-center space-y-4 sm:space-y-6 md:space-y-8 lg:space-y-10 w-full px-4 md:px-8 lg:px-16">
+        <p className="text-center text-white text-4xl md:text-5xl lg:text-7xl font-bold leading-snug sm:leading-normal md:leading-relaxed">
+          Wishful Thinking spreads inspiration through dialogue & vision,
+          creating a sign of hope and placing your mind in a happy place no
+          matter where you stand.
         </p>
-        <p className="text-center text-white text-6xl md:text-8xl font-bold">
-          Positivity You Spread.
+        <p className="text-center text-white text-4xl md:text-5xl lg:text-7xl font-bold leading-snug sm:leading-normal md:leading-relaxed">
+          Wishful Thinking is Motivation You Wear & Positivity you spread!
         </p>
       </div>
     </div>
--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -9,13 +9,14 @@ const Hero = () => {
         autoPlay
         loop
         muted
+        playsInline
         className="absolute z-0 w-auto min-w-full min-h-full max-w-none"
       >
         <source src={Sky} type="video/mp4" />
       </video>
       <div className="flex flex-col items-center justify-center z-10 h-full w-full md:justify-start">
         <img className="object-contain mb-4 md:mb-4" src={Logo} alt="Logo" />
-        <button className="bg-white hover:bg-[#b1a482] text-gray-800 font-bold py-3 px-5 text-lg -mt-[100px] md:-mt-[220px] sm:py-3 sm:px-6 sm:text-xl md:py-3 md:px-6 md:text-xl lg:py-4 lg:px-8 lg:text-2xl xl:py-4 xl:px-8 xl:text-2xl rounded-full">
+        <button className="bg-white sm:hover:bg-white hover:bg-[#b1a482] text-gray-800 font-bold py-3 px-5 text-lg -mt-[100px] md:-mt-[220px] sm:py-3 sm:px-6 sm:text-xl md:py-3 md:px-6 md:text-xl lg:py-4 lg:px-8 lg:text-2xl xl:py-4 xl:px-8 xl:text-2xl rounded-full">
           Fall 23 Collection
         </button>
       </div>
