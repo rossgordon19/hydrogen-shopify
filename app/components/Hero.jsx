7120955b9fd06c0f937632c932b74111599c18a2
Ross Gordon <rossagordonstl@gmail.com>
2023-08-15 19:54:29 -0400

    final cta fix

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-scroll';
+import {Link} from 'react-scroll';
 import Logo from '../assets/logo.png';
 import Sky from '../assets/sky1.mov';
 
@@ -16,12 +16,16 @@ const Hero = () => {
         <source src={Sky} type="video/mp4" />
       </video>
       <div className="flex flex-col items-center justify-center z-10 h-full w-full lg:justify-center">
-        <img className="object-contain mb-4 lg:mb-[100px]" src={Logo} alt="Logo" />
-        <Link to="products" smooth={true} duration={500}>
-          <button className="bg-white sm:hover:bg-white text-gray-800 font-bold py-3 px-5 text-lg sm:py-3 sm:px-6 sm:text-xl md:py-3 md:px-6 md:text-xl lg:py-4 lg:px-8 lg:text-2xl xl:py-4 xl:px-8 xl:text-2xl rounded-full">
-            Fall 23 Collection
-          </button>
-        </Link>
+        <img className="object-contain" src={Logo} alt="Logo" />
+        <div className="mt-[-100px] md:mt-[-200px]">
+          {' '}
+          {/* Negative margin */}
+          <Link to="products" smooth={true} duration={500}>
+            <button className="bg-white text-gray-800 font-bold py-3 px-5 text-lg sm:py-3 sm:px-6 sm:text-xl md:py-3 md:px-6 md:text-xl lg:py-4 lg:px-8 lg:text-2xl xl:py-4 xl:px-8 xl:text-2xl rounded-full">
+              Fall 23 Collection
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
