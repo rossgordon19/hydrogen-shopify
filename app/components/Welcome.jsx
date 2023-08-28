ab9ba532674de77de0b1927b68cd4e5cc33d016a
Ross Gordon <rossagordonstl@gmail.com>
2023-08-27 20:49:36 -0400

    update video background

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, {useRef, useEffect} from 'react';
 import Shirts from '../assets/summershirts.mp4';
 
 const Welcome = () => {
@@ -11,7 +11,7 @@ const Welcome = () => {
   }, []);
 
   return (
-    <div className="relative w-full h-[50vh] md:h-screen flex items-center justify-center overflow-hidden bg-black">
+    <div className="relative w-full h-[50vh] md:h-screen flex items-center justify-center overflow-hidden bg-white text-black">
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="w-full h-full relative">
           <video
@@ -20,14 +20,14 @@ const Welcome = () => {
             loop
             muted
             playsInline
-            className="absolute inset-0 w-full h-full object-contain"
+            className="absolute inset-0 w-full h-full object-cover" // Changed from object-contain to object-cover
           >
             <source src={Shirts} type="video/mp4" />
           </video>
         </div>
       </div>
-      <div className="absolute inset-x-0 top-0 flex flex-col items-center justify-start z-10 text-white text-center space-y-4 pt-4">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold">
+      <div className="absolute inset-x-0 top-0 flex flex-col items-center justify-start z-10 text-center space-y-4 pt-4">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-white">
           End of Summer 23 Collection
         </h1>
       </div>
